895ea3b60074c72fe74df0acafa71653312480ba
khanhnd-vinsofts <khanhnd@vinsofts.net>
2018-09-04 16:22:28 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import ViewBackgroundWeather from './app/screens/ViewBackgroundWeather';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <ViewBackgroundWeather  style={{flex: 1}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+});
---
+++ app/screens/ViewBackgroundWeather.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import ViewWeatherNow from '../components/ViewWeatherNow';
+
+
+const image = require('../images/fog_bg.jpg')
+export default class ViewBackgroundWeather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+        <ImageBackground
+          style={{ width: '100%', height: '100%' }}
+          source={uri = image}
+        >
+          <ViewWeatherNow />
+        </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+       
+
+    }
+})
