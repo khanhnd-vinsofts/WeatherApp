ad724cf0da91e4a5ad6399a8a97aff68780b6e16
khanhnd-vinsofts <khanhnd@vinsofts.net>
2018-09-06 09:26:31 +0700

    Auto stash before merge of "develop" and "origin/develop"
    edit firstHourWeather 09:26 06/09/2017

--- app/screens/ViewTemperatureTime.js
+++ app/screens/ViewTemperatureTime.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import ViewWeatherNow from '../components/WeatherToday/ViewWeatherNow';
+import FirstHourWeather from '../components/WeatherTimeToday/firstHourWeather';
+import LineChartWeather from '../components/WeatherTimeToday/lineChartWeather';
 
 
 export default class ViewTemperatureTime extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-  render() {
-    return (
-      <View style={styles.wrapper}>
-
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.wrapper}>
+                <View style={styles.layout}>
+                    <View style={styles.hourWeather}>
+                       <FirstHourWeather />
+                    </View>
+                    <View style={styles.lineChartWeather}>
+                        
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     wrapper: {
-        flex: 1,
+        flex: 1,     
+    },
+    layout: {
+        marginTop: 102,
+        width: 350,
+        height: 200,
+        borderRadius: 3,
+        flexDirection: 'column',
+        backgroundColor: '#000000',
+        opacity: 0.35,
+        
+    },
+    hourWeather: {
+        flex: 1,    
+        
+    },
+    lineChartWeather: {
+        flex: 2,    
     }
 })
