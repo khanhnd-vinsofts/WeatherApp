a44331b108e8b3890f5dc3b9811695d698c23e45
tungvins <tungtruong@vinsofts.net>
2018-09-07 13:32:12 +0700

    Auto stash before merge of "develop" and "thanhtung"
    edit layout screen TemperatureDay 13h30 07/09/2018

--- app/components/WeatherNextDay/WeatherNextDay.js
+++ app/components/WeatherNextDay/WeatherNextDay.js
@@ -1,63 +1,114 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, FlatList, ActivityIndicator, Image } from 'react-native';
-import data from '../../server/HorizontalData.json';
 
 export default class WeatherNextDay extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: [
-
-            ],
+            listData: [
+                {
+                    name: "TH 2",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 11
+                },
+                {
+                    name: "TH 3",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 90
+                },
+                {
+                    name: "TH 4",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 98
+                },
+                {
+                    name: "TH 5",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 76
+                },
+                {
+                    name: "TH 6",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 9
+                },
+                {
+                    name: "TH 7",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 12
+                },
+                {
+                    name: "CN",
+                    image: require('../../images/icon/ic_sun_min.png'),
+                    icon: require('../../images/icon/ic_rain_probability.png'),
+                    ratio: 14
+                }
+            ]
         };
-    }
-
-    componentWillMount = () => {
-        this.setState({
-            data: data.weather_of_the_day,
-        });
     }
 
     render() {
         return (
             <View style={styles.constainer}>
-                <View style={{ height: 80 }}>
+                <View>
                     <View style={{ paddingTop: 7 }}>
                         <Text style={{ color: "#ffffff", fontSize: 12, textAlign: 'center', fontStyle: 'italic' }}>Chạm vào một ngày để xem dự báo theo giờ</Text>
                     </View>
                     <FlatList
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
-                        data={this.state.data}
+                        data={this.state.listData}
                         keyExtractor={(item, index) => index.toString()}
                         renderItem={({ item }) => {
                             return (
                                 <View style={{
                                     marginTop: 10,
-                                    flex: 1,
                                     flexDirection: 'column',
                                     alignItems: 'center',
                                     width: 70,
                                 }}>
-                                    <Text style={{
-                                        fontSize: 15,
-                                        color: 'white',
-                                        paddingHorizontal: 13,
-                                        paddingVertical: 5,
-                                        textDecorationLine: "underline",
+                                    <View style={{
+                                        width: 50,
+                                        height: 30,
                                         borderWidth: 1,
                                         borderColor: '#ffffff',
                                         borderRadius: 5,
-                                        fontWeight: 'bold'
+                                        justifyContent: 'center',
+                                        alignItems: 'center'
                                     }}>
-                                        {item.the_day_of_weak}
-                                    </Text>
-                                    <View>
-                                        <Image
-                                            source={{ uri: item.icon }}
-                                        />
+                                        <Text style={{
+                                            fontSize: 15,
+                                            color: 'white',
+                                            textDecorationLine: "underline",
+                                            fontWeight: 'bold'
+                                        }}>
+                                            {item.name}
+                                        </Text>
                                     </View>
+                                    <Image style={{
+                                        width: 20,
+                                        height: 20,
+                                        marginTop:5
+                                    }} source={item.image}></Image>
+                                    <Text style={{
+                                        fontSize: 9,
+                                        color: 'white',
+                                    }}>
+                                        <Image 
+                                            style={{
+                                                width: 12,
+                                                height: 15,
+                                            }}
+                                            source={item.icon}
+                                        />
+                                        {item.ratio}%
+                                    </Text>
                                 </View>
                             );
                         }}
