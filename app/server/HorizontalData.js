aa0217c46926578a435a618e034b2ddc0dc90cb9
tungvins <tungtruong@vinsofts.net>
2018-09-15 13:40:17 +0700

    update view layout 13h40 15/09/2018

--- app/server/HorizontalData.js
+++ app/server/HorizontalData.js
@@ -1,4 +1,4 @@
-const getWeather = function(){
+const getWeather = () => {
     var url =`https://api.openweathermap.org/data/2.5/forecast?q=Hanoi,VN&appid=e870504be89c778223b86a5adf8d4abd`;
     return fetch(url).then((res) =>res.json());
 }
